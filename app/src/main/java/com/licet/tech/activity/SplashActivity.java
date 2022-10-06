7f56aa1c9a5d6c34b31dd11b745ea1b0a30beb7a
limson <limsonts96@gmail.com>
2022-10-06 23:29:26 +0530

    added fire base integration

--- app/src/main/java/com/licet/tech/activity/SplashActivity.java
+++ app/src/main/java/com/licet/tech/activity/SplashActivity.java
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.licet.tech.MainActivity;
 import com.licet.tech.R;
 
-public class SplashActivity extends AppCompatActivity {
 
+public class SplashActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        /**Fire base intialization*/
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         new Thread(new Runnable() {
             @Override
             public void run() {
